3425ce684c5cb5d26d2431f0f8535d00f6bdd1bb
CrisGonzaVal <cgonzalezvalenzuela4@gmail.com>
2024-11-26 04:55:17 -0300

    ultimos cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //enlazar con android
-  apiUrl:'http://localhost:3000'
- //apiUrl:'https://repodata-4x18.onrender.com'
+  //apiUrl:'http://localhost:3000'
+ apiUrl:'https://repodata-rqrl.onrender.com'
 
 };
